13df9cff742a0d3471f186a25b6a0b3983784443
Tobias Schramm <tobleminer@gmail.com>
2017-09-07 21:15:01 +0200

    Add constructor arguments

--- mixin.js
+++ mixin.js
@@ -1,13 +1,17 @@
 'use strict';
 
 module.exports = function(...args) {
-	let mix = function(classes) { 
+	let mix = function(classes) {
 		return class {
-			constructor()
+			constructor(...cargs)
 			{
 				let clazz;
 				while((clazz = classes.pop())) {
-					let sub = new clazz();
+					let largs = cargs.slice();
+					largs.unshift(clazz);
+					let sub = new
+						(Function.prototype.bind
+							.apply(clazz, largs));
 					Object.getOwnPropertyNames(sub).forEach(key => {
 						this[key] = sub[key];
 					});
@@ -21,8 +25,9 @@ module.exports = function(...args) {
 		Object.getOwnPropertyNames(clazz.prototype)
 			.filter(key => key !== 'constructor')
 			.forEach(key => {
-			mix.prototype[key] = clazz.prototype[key];
-		});
+				mix.prototype[key] = clazz.prototype[key];
+			}
+		);
 	}
 
 	return mix;
