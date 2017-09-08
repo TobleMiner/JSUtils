84b66285cdb42556205c08a9d3dcbb8684788445
Tobias Schramm <tobleminer@gmail.com>
2017-09-08 09:07:42 +0200

    Add some comments explaining the crazier parts of the mixin code

--- mixin.js
+++ mixin.js
@@ -7,8 +7,18 @@ module.exports = function(...args) {
 			{
 				let clazz;
 				while((clazz = classes.shift())) {
+					/* Split of arguments that will be consumed
+					 * by constructor of clazz
+					 */
 					let largs = cargs.splice(0, clazz.length);
+					// Preprend clazz as this-arg for constructor
 					largs.unshift(clazz);
+					/* Use function prototype to replace this-arg
+					 * of bind method with clazz. This allows to
+					 * bind arguements to a constructor, yielding
+					 * a bound constructor that can be invoked
+					 * with new
+					 */
 					let sub = new
 						(Function.prototype.bind
 							.apply(clazz, largs));
