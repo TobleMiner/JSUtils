23749da39eb8f9e1feb4f1e24d345b9f392480ff
Tobias Schramm <tobleminer@gmail.com>
2017-09-16 09:50:33 +0200

    Import subclass functions into prototype of parent
    Clone prototype of parent on instantiation and populate it with functions form child so they can be called from parents constructor

--- mixin.js
+++ mixin.js
@@ -1,41 +1,74 @@
 'use strict';
 
+const argCharset = 'abcdeghijklmnopqrstuvwxyz';
+
+// Generates comma seperated argument list
+function generateArgString(count) {
+	let argString = '';
+	let prefix = '';
+	let charsetPos = 0;
+	while(count-- > 0) {
+		let suffix = ',';
+		if(!count)
+			suffix = '';
+		argString += prefix + argCharset[charsetPos] + suffix;
+		if(++charsetPos == argCharset.length) {
+			prefix += argCharset[0];
+			charsetPos = 0;
+		}
+	}
+	return argString;
+}
+
+/*
+ * Get full list of all inherited property names excluding constructors
+ * and ones from Object
+ */
+function getInheritedPropertyNames(proto) {
+	let propNames = [];
+	while(Object.getPrototypeOf(proto) && proto !== Object.prototype) {
+		propNames = propNames.concat(Object.getOwnPropertyNames(proto));
+		proto = Object.getPrototypeOf(proto);
+	}
+	return propNames.filter(key => key !== 'constructor');
+}
+
 module.exports = function(...args) {
-	let mix = function(classes) {
+	// Generate list of arguments dynamically to get correct constructor.length
+	let nArgs = args.reduce(((count, clazz) => count + clazz.length), 0);
+	let argString = generateArgString(nArgs);
+	let mix = new Function('classes, getInheritedPropertyNames', `
 		return class {
-			constructor(...cargs)
+			constructor(${argString})
 			{
+				let cargs = [${argString}];
 				classes.forEach(clazz => {
-					/* Split of arguments that will be consumed
-					 * by constructor of clazz
-					 */
+					// Clone and expand prototype
+					let obj = Object.create(clazz.prototype);
+
+					// Populate cloned prototype
+					getInheritedPropertyNames(this.__proto__)
+                                                .forEach(prop =>
+							obj.__proto__[prop] = this.__proto__[prop]);
+
 					let largs = cargs.splice(0, clazz.length);
-					// Preprend clazz as this-arg for constructor
 					largs.unshift(clazz);
-					/* Use function prototype to replace this-arg
-					 * of bind method with clazz. This allows to
-					 * bind arguements to a constructor, yielding
-					 * a bound constructor that can be invoked
-					 * with new
-					 */
+					// Create new object from constructor of populated prototype
 					let sub = new
 						(Function.prototype.bind
-							.apply(clazz, largs));
+							.apply(obj.__proto__.constructor, largs));
 					Object.getOwnPropertyNames(sub).forEach(key => {
 						this[key] = sub[key];
 					});
 				});
 			}
-		}
-	}(args.slice());
+		}`
+	)(args, getInheritedPropertyNames);
 
+	// Copy over functions
 	args.forEach(clazz => {
-		Object.getOwnPropertyNames(clazz.prototype)
-			.filter(key => key !== 'constructor')
-			.forEach(key => {
-				mix.prototype[key] = clazz.prototype[key];
-			}
-		);
+		getInheritedPropertyNames(clazz.prototype).forEach(key =>
+				mix.prototype[key] = clazz.prototype[key]);
 	});
 
 	return mix;
